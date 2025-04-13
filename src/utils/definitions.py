a96707ac7c0f83847068de23b74da27987139e1c
DCajiao <dcajiaolazt@gmail.com>
2025-04-13 14:08:50 -0500

    ✨Feat: add database info section and ensure consistent formatting in definitions.py

--- src/utils/definitions.py
+++ src/utils/definitions.py
@@ -5,3 +5,6 @@
 RENDER_LOGIN_URL = "https://dashboard.render.com/login"
 RENDER_CREATE_NEW_DATABASE_URL = "https://dashboard.render.com/new/database"
 RENDER_URL = "https://dashboard.render.com"
+
+# DATABASE INFO
+DB_MAX_HOURS = 750
