e03b7945f21083670491e0e3798c342acb8e3c92
DCajiao <dcajiaolazt@gmail.com>
2025-04-13 12:24:25 -0500

    ✨Feat: add initial logging setup in main.py for improved monitoring

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#from flask import Flask, request
+import logging
+import time
+import core.scrapper as scrapper
+
+
+#### ---------- LOGGING ---------- ####
+logging.basicConfig(
+    format='%(asctime)s - %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+   pass
